8d4b46c933960a9282d958e8715f879085c970cb
AldricVS <aldric.silvestre@outlook.fr>
2022-06-14 19:03:45 +0200

    Fix numcConvertUtils corrdinates functions

--- src/util/numconvert_utils.cpp
+++ src/util/numconvert_utils.cpp
@@ -1,13 +1,24 @@
 #include "numconvert_utils.h"
 
 #include <sstream>
+#include <math.h>
+#include <iomanip>
+#include <iostream>
 
 std::string numConvertUtils::coordIntToString(int coordinate)
 {
+    // Treat the "negative" part afterwards
+    bool isNegative = coordinate < 0;
+    coordinate = abs(coordinate);
     int integerPart = coordinate / 10000000;
     int decimalPart = coordinate % 10000000;
     std::stringstream ss;
-    ss << integerPart << "." << decimalPart;
+    if (isNegative)
+    {
+        ss << "-";
+    }
+    // format decimal part with leading zeroes
+    ss << integerPart << "." << std::setfill('0') << std::setw(7) << decimalPart;
     return ss.str();
 }
 
